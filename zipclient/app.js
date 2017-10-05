4073351e337af9264f965fae6487f3121a09d194
Emily Zhai <ezhai24@gmail.com>
2017-10-05 13:46:55 -0700

    web server and client

--- zipclient/app.js
+++ zipclient/app.js
@@ -1,9 +1,25 @@
-setInterval(() => {
-  fetch('http://localhost:4000/memory')
+var cityForm = document.getElementById('city-form');
+cityForm.addEventListener("submit", e => {
+  e.preventDefault();
+ 
+  var city = document.getElementById('city').value;
+  var states = new Set();
+  var codes = new Set();
+  fetch('http://localhost:4001/zips/' + city)
   .then((response) => {
     return response.json()
   })
   .then((data) => {
-    document.getElementById('alloc').innerHTML = 'Allocated Memory: ' + data.Alloc
+    data.forEach((zip) => {
+      codes.add(zip.Code);
+      states.add(zip.State);
+    })
   })
-}, 1000);
+
+  var statesSelect = document.getElementById('states');
+  states.forEach((state) => {
+    var option = document.createElement("option");
+    option.setAttribute("value", state);
+    statesSelect = statesSelect.appendChild(option)
+  })
+})
